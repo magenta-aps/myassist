9e6df3851427b41f91de3bb7213b1ef2f8169dc9
Lars Peter Thomsen <larsp@magenta.dk>
2015-04-30 11:31:15 +0200

    Exported fields

--- sites/all/themes/myassist_theme/templates/profile2.tpl.php
+++ sites/all/themes/myassist_theme/templates/profile2.tpl.php
@@ -1,35 +1,42 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
 <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>>
-        <a href="<?php print $url; ?>"><?php print $title; ?></a>
-    </h2>
+  <h2<?php print $title_attributes; ?>>
+    <a href="<?php print $url; ?>"><?php print $title; ?></a>
+  </h2>
 <?php endif; ?>
 
 
-    <?php
-        $left_blocks = array("field_image","points","level","achievements");
-    ?>
+  <?php
+  $left_blocks = array("field_image","points","level","achievements");
+  $right_blocks = array("username","field_gender","field_age","membersince","field_quote");
+  ?>
 
-    <aside class="left">
-        <section class="region column sidebar region-sidebar-first">
-            <?php
-            foreach ($left_blocks as $blockname) {
-                if (array_key_exists($blockname, $content)) {
-                    print render($content[$blockname]);
-                }
-            }
-            ?>
-        </section>
-    </aside>
-
-    <div class="content"<?php print $content_attributes; ?>>
-        <?php
-        foreach ($content as $blockname => $blockvalue) {
-            if (!in_array($blockname, $left_blocks)) {
-                print render($blockvalue);
-            }
+  <aside class="left">
+    <section class="region column sidebar region-sidebar-first">
+      <?php
+      foreach ($left_blocks as $blockname) {
+        if (array_key_exists($blockname, $content)) {
+          print render($content[$blockname]);
         }
-        ?>
-    </div>
+      }
+      ?>
+    </section>
+  </aside>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+    foreach ($right_blocks as $blockname) {
+      if (array_key_exists($blockname, $content)) {
+        print render($content[$blockname]);
+      }
+    }
+    
+    foreach ($content as $blockname => $blockvalue) {
+      if (!in_array($blockname, $left_blocks) && !in_array($blockname, $right_blocks)) {
+        print render($blockvalue);
+      }
+    }
+    ?>
+  </div>
 </div>
