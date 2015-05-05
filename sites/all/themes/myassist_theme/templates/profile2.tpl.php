9930908c95b1309e3ba90c5c352dc8220aba02b5
Lars Peter Thomsen <larsp@magenta.dk>
2015-05-05 13:14:00 +0200

    New profile layout

--- sites/all/themes/myassist_theme/templates/profile2.tpl.php
+++ sites/all/themes/myassist_theme/templates/profile2.tpl.php
@@ -10,6 +10,8 @@
   <?php
   $left_blocks = array("field_avatar","username","points","level");
   $right_blocks = array("achievements","userinfo","membersince","field_quote");
+
+  $rendered_blocks = array();
   ?>
 
   <div class="section">
@@ -17,8 +19,9 @@
     <div class="left">
         <?php
         foreach ($left_blocks as $blockname) {
-          if (array_key_exists($blockname, $content)) {
+          if (array_key_exists($blockname, $content) && !in_array($blockname, $rendered_blocks)) {
             print render($content[$blockname]);
+            $rendered_blocks[] = $blockname;
           }
         }
         ?>
@@ -27,8 +30,9 @@
     <div class="right"<?php print $content_attributes; ?>>
       <?php
       foreach ($right_blocks as $blockname) {
-        if (array_key_exists($blockname, $content)) {
+        if (array_key_exists($blockname, $content) && !in_array($blockname, $rendered_blocks)) {
           print render($content[$blockname]);
+          $rendered_blocks[] = $blockname;
         }
       }
       ?>
@@ -36,13 +40,22 @@
 
   </div>
 
-  <div class="section">
-    <?php
-      foreach ($content as $blockname => $blockvalue) {
-        if (!in_array($blockname, $left_blocks) && !in_array($blockname, $right_blocks)) {
-          print render($blockvalue);
-        }
-      }
-    ?>
-  </div>
+  <?php
+    if (array_key_exists("nodes", $content) && !in_array("nodes", $rendered_blocks)) {
+      ?>
+      <div class="section nodes">
+        <h3>Aktiviteter</h3>
+        <?php
+          print render($content["nodes"]);
+        ?>
+      </div>
+      <?php
+    }
+        /*foreach ($content as $blockname => $blockvalue) {
+          if (!in_array($blockname, $rendered_blocks)) {
+            print render($blockvalue);
+            $rendered_blocks[] = $blockname;
+          }
+        }*/
+  ?>
 </div>
