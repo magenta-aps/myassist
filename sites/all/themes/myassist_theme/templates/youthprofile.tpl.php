66cbfc40e54e7f864062445eea243ead52703f80
Test <seth@magenta.dk>
2015-05-18 14:17:40 +0000

    change layout for profile and marked bedst answers

--- sites/all/themes/myassist_theme/templates/youthprofile.tpl.php
+++ sites/all/themes/myassist_theme/templates/youthprofile.tpl.php
@@ -1,26 +1,14 @@
 
 <?php
-$left_blocks = array("field_avatar","username","points","level","field_expertise");
-$right_blocks = array("achievements","userinfo","membersince","field_quote");
+$blocks = array("field_avatar","username","points","level","field_expertise","achievements","userinfo","membersince","field_quote");
 $rendered_blocks = array();
 ?>
 
 <div class="section">
 
-  <div class="left">
+  <div <?php print $content_attributes; ?>>
     <?php
-    foreach ($left_blocks as $blockname) {
-      if (array_key_exists($blockname, $content) && !in_array($blockname, $rendered_blocks)) {
-        print render($content[$blockname]);
-        $rendered_blocks[] = $blockname;
-      }
-    }
-    ?>
-  </div>
-
-  <div class="right"<?php print $content_attributes; ?>>
-    <?php
-    foreach ($right_blocks as $blockname) {
+    foreach ($blocks as $blockname) {
       if (array_key_exists($blockname, $content) && !in_array($blockname, $rendered_blocks)) {
         print render($content[$blockname]);
         $rendered_blocks[] = $blockname;
