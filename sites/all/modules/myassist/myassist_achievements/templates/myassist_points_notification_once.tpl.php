6f41fe629ac2b4956b84af5127b260bbbad203ad
Lars Peter Thomsen <larsp@magenta.dk>
2015-05-27 12:00:11 +0200

    Audio feedback for point notifications

---
+++ sites/all/modules/myassist/myassist_achievements/templates/myassist_points_notification_once.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for a one-shot notification popup.
+ *
+ */
+?>
---
+++ sites/all/themes/myassist_theme/templates/myassist_points_notification_once.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Myassist theme implementation for a one-shot notification popup.
+ *
+ * When notifications are displayed, this template gets displayed as well,
+ * but while there may be any number of notifications, this template only displays once per request
+ */
+?>
+<audio autoplay>
+  <source src="/sites/all/themes/myassist_theme/sounds/sound4.mp3" type="audio/mpeg"/>
+</audio>
