3387901859931ffc71442b384dcef58ed53c9544
Test <seth@magenta.dk>
2015-06-15 13:54:57 +0000

    More responsive styles

--- sites/all/themes/myassist_theme/templates/page.tpl.php
+++ sites/all/themes/myassist_theme/templates/page.tpl.php
@@ -11,43 +11,45 @@
 <div id="page">
 
   <header class="header" id="header" role="banner">
+    <div class="header__container">
+      
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+      <?php endif; ?>
+      
+      <?php if ($site_name || $site_slogan): ?>
+        <div class="header__name-and-slogan" id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <h1 class="header__site-name" id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+  
+          <?php if ($site_slogan): ?>
+            <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
+      
+      <?php print render($page['header']); ?>
+      
+      <?php if ($secondary_menu): ?>
+        <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+          <?php print theme('links__system_secondary_menu', array(
+            'links' => $secondary_menu,
+            'attributes' => array(
+              'class' => array('links', 'inline', 'clearfix'),
+            ),
+            'heading' => array(
+              'text' => $secondary_menu_heading,
+              'level' => 'h2',
+              'class' => array('element-invisible'),
+            ),
+          )); ?>
+        </nav>
+      <?php endif; ?>
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-    <?php endif; ?>
-    
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div>
-    <?php endif; ?>
-    
-    <?php print render($page['header']); ?>
-    
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
+    </div>
   </header>
 
   <div id="main">
@@ -97,9 +99,13 @@
   </div>
   
   <?php print render($page['navigation']); ?>
-
+  
   <?php print render($page['footer']); ?>
 
+</div>
+
+<div class="bottom__container">
+  <?php print render($page['bottom']); ?>
 </div>
 
 
@@ -115,6 +121,3 @@
   };
 </script>
 <!-- End Feedbackr.js resources -->
-
-
-<?php print render($page['bottom']); ?>
