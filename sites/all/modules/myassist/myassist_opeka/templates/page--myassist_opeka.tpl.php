fa5439a1080b4eb95396e5b9ef3a61772f9ac1ca
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2015-07-01 14:05:20 +0200

    - Only render the chat and nothing else when viewing chat pages.

---
+++ sites/all/modules/myassist/myassist_opeka/templates/page--myassist_opeka.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Custom HTML used for displaying a full myassist_opeka HTML page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="page">
+    <div id="main">
+        <div id="content" class="column" role="main">
+            <a id="main-content"></a>
+            <?php print render($page['content']); ?>
+        </div>
+    </div>
+</div>
