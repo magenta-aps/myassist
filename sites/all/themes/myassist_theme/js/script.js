569d30044ab2159af9502bad0d066a52d11841c0
Test <seth@magenta.dk>
2015-08-11 07:08:11 +0000

    javascript fix

--- sites/all/themes/myassist_theme/js/script.js
+++ sites/all/themes/myassist_theme/js/script.js
@@ -10,16 +10,18 @@
 // wrapping it with an "anonymous closure". See:
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+
+
 (function ($, Drupal, window, document, undefined) {
-
-// To understand behaviors, see https://drupal.org/node/756722#behaviors
-
-
+  
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  
   Drupal.behaviors.headerResponsive = {
     attach: function(context, settings) {
       
       // Toggle user action menu
       $('#block-system-user-menu .block-title').click(function() {
+        
         $('#block-system-user-menu').toggleClass('open');
       });
       
@@ -27,39 +29,12 @@
       $('#block-search-form .block-title').click(function() {
         $('#block-search-form').toggleClass('open');
       });
-
-    }
-  };
-
-  var updateOffset = function() {
-    var offset = $("#toolbar").height();
-    if (offset && !isNaN(offset)) {
-      $("#page > *").each(function(){
-        var el = $(this);
-        if (el.css("position") === "fixed") {
-          el.offset({top:offset});
-        }
-      });
-
-    }
-  };
-
-  Drupal.behaviors.toolbarOffset = {
-    attach: function(context, settings) {
-      $('#toolbar a.toggle', context).click(updateOffset);
-    }
-  };
-
-  $(updateOffset);
-  $(window).resize(updateOffset);
-  
-})(jQuery, Drupal, this, this.document);
-
-
-(function ($, Drupal, window, document, undefined) {
-  Drupal.behaviors.headerResponsive = {
-    attach: function(context, settings) {
-
+      
+      
+      /*
+       * Message Form script
+       */
+      
       //var messageForm = $("#answers-answer-node-form");
       //var messageField = messageForm.find("textarea[id^=edit-body]");
 
@@ -103,5 +78,27 @@
       });
     }
   };
+  
+  var updateOffset = function() {
+    var offset = $("#toolbar").height();
+    if (offset && !isNaN(offset)) {
+      $("#page > *").each(function(){
+        var el = $(this);
+        if (el.css("position") === "fixed") {
+          el.offset({top:offset});
+        }
+      });
+
+    }
+  };
+
+  Drupal.behaviors.toolbarOffset = {
+    attach: function(context, settings) {
+      $('#toolbar a.toggle', context).click(updateOffset);
+    }
+  };
+
+  $(updateOffset);
+  $(window).resize(updateOffset);
 
 })(jQuery, Drupal, this, this.document);
