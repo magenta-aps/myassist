34cbf7627dd45f2054f139df3424f0f00716e7a5
Lars Peter Thomsen <larsp@magenta.dk>
2016-04-04 15:09:29 +0200

    Migrate config from prod to testing: MyAssist Theme

---
+++ sites/all/themes/myassist_theme/js/GA_user_registration_conversion.js
@@ -0,0 +1,9 @@
+/* Google Code for Oprettelse af bruger Conversion Page */
+
+var google_conversion_id = 937390709;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "Lqs_CK_EumEQ9eT9vgM"; var google_remarketing_only = false;
+
+
