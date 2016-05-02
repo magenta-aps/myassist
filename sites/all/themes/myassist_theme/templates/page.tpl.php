f5947533ff9baab9d63bfdf0e589cef1d81a0796
Lars Peter Thomsen <larsp@magenta.dk>
2016-05-02 16:37:25 +0200

    Remove duplicate

--- sites/all/themes/myassist_theme/templates/page.tpl.php
+++ sites/all/themes/myassist_theme/templates/page.tpl.php
@@ -58,7 +58,6 @@
 
   <div id="main">
     <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
